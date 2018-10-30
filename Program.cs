1ed4289eb89f410b72e8225226b87f73be0f14ab
ssmith2 <ssmith2@eecs.wsu.edu>
2018-10-29 20:39:25 -0700

    Intial version of compress and file parsing functions

--- Program.cs
+++ Program.cs
@@ -9,63 +9,41 @@ namespace ppcalc
 {
     class Program
     {
-        //static StreamWriter sw;
-
         static void Main(string[] args)
         {
-            //StreamWriter sw = new StreamWriter("local_BRCA2.txt");
-            if (args.Count() != 3)
+            if (args.Count() != 2)
             {
                 Console.WriteLine("You done messed up. Use the following form when calling this executable:");
-                Console.WriteLine("$ <test executable> <input file containing sequence s> <input alphabet file>");
-                return;
+                Console.WriteLine("$<input file containing the map>");
             }
-            SuffixTrie trie = new SuffixTrie(getInput(args[1]), getAlphabet(args[2]));
+            SuffixTrie trie = new SuffixTrie(compress(buildArray(args[1])));
 
-            //do the things
+            //do the analysis
+            //output results
         }
 
-        static string getInput(string fileName)
+        static Note[] buildArray(string fileName)
         {
             string line;
-            string retVal = "";
+            Note[] notes = {};
             using (StreamReader file = new StreamReader(fileName))
             {
-                Console.Write(fileName + "\n");
                 while ((line = file.ReadLine()) != null)
                 {
-                    if (line == "")
-                    {
-                        break;
-                    }
-                    else if (line[0] != '>')
-                    {
-                        retVal += line.Trim();
-                    }
+                    //create notes tbd
+                    notes.append(new Note());
                 }
             }
-            return retVal += "$";
+            return notes;
         }
 
-        static string getAlphabet(string alphabetFileName) {
-            string line;
-            string retVal = "$ ";
-            using (StreamReader file = new StreamReader(alphabetFileName))
-            {
-                Console.Write(alphabetFileName + "\n");
-                while ((line = file.ReadLine()) != null)
-                {
-                    if (line == "")
-                    {
-                        break;
-                    }
-                    else
-                    {
-                        retVal += line.Trim();
-                    }
+        Note[] compress(Note[] notes) {
+            for (int i = notes.size(); i > 0; i--;) {
+                if (notes[i] = notes[i - 1]) {
+                    notes[i - 1].quantity = notes[i].quantity++;
+                    notes.removeAt(i);
                 }
-            }
-            return retVal;
+            } 
         }
     }
 }
