5a3f9c053149a29525cc1adf94207eddf0ed21df
ssmith2 <ssmith2@eecs.wsu.edu>
2018-11-10 17:03:19 -0800

    implement file parsing

--- Program.cs
+++ Program.cs
@@ -28,17 +28,26 @@ namespace ppcalc
             Note[] notes = {};
             using (StreamReader file = new StreamReader(fileName))
             {
+                while ((line = file.ReadLine()) != null && line != "[HitObjects]")
+                {
+                }
+
                 while ((line = file.ReadLine()) != null)
                 {
-                    //create notes tbd
-                    notes.append(new Note());
+                    string[] noteData = line.Split(',');
+                    notes.append(new Note(Int32.Parse(noteData[2]), 1, Int32.Parse(noteData[3]) & 11));
                 }
             }
+            for (int i = notes.length; i > 0; i--)
+            {
+                notes[i - 1].duration = notes[i].duration - notes[i - 1].duration;
+            }
+            notes[notes.length - 1].duration = 0;
             return notes;
         }
 
         Note[] compress(Note[] notes) {
-            for (int i = notes.size(); i > 0; i--;) {
+            for (int i = notes.length; i > 0; i--;) {
                 if (notes[i] = notes[i - 1]) {
                     notes[i - 1].quantity = notes[i].quantity++;
                     notes.removeAt(i);
