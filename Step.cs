770156105d5e94d00e1d5399ee5c729fb54aa566
ssmith2 <ssmith2@eecs.wsu.edu>
2018-11-10 16:34:14 -0800

    added note types to note class

--- Step.cs
+++ Step.cs
@@ -1,19 +1,27 @@
 ﻿namespace ppcalc
 {
+    enum NoteType 
+    {
+        circle, slider, bouncer, spinner
+    }
+
     class Note
     {
+        NoteType type;
         double duration;
         int quantity;
 
-        public Note(doublt _duration, int _quantity) {
+        public Note(doublt _duration, int _quantity, NoteType _type) {
             duration = _duration;
             quantity = _quantity;
+            type = _type;
         }
 
         public override bool Equals(object obj)
         {
             Step rhs = (Step)obj;
-            return this.duration == rhs.duration && this.quantity == rhs.quantity;
+            return this.duration == rhs.duration && this.quantity == rhs.quantity 
+                                    && this.type = rhs.type;
         }
 
         /*public static bool operator == (Step lhs, Step rhs)
