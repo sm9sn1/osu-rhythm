bc37f483d013ee79c57f9d656e09a27b584689a3
ssmith2 <samuel.e.smith@wsu.edu>
2018-11-18 16:15:20 -0800

    Migration from VS code to VS community

---
+++ osu-rhythmic-complexity/osu-rhythmic-complexity/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ppcalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO: use CLI for parameters instead of argv
+            if (args.Count() != 2)
+            {
+                Console.WriteLine("You done messed up. Use the following form when calling this executable:");
+                Console.WriteLine("$<input file containing the map>");
+            }
+            SuffixTrie trie = new SuffixTrie(compress(buildArray(args[1])));
+
+            //do the analysis
+            //output results
+        }
+
+        static Note[] buildArray(string fileName)
+        {
+            string line;
+            Note[] notes = {};
+            using (StreamReader file = new StreamReader(fileName))
+            {
+                //skip past metadata to notes section
+                while ((line = file.ReadLine()) != null && line != "[HitObjects]")
+                {
+                }
+                //1 notes per line, comma delineated
+                while ((line = file.ReadLine()) != null)
+                {
+                    string[] noteData = line.Split(',');
+                    //note type is stored as a bitmap
+                    notes.append(new Note(Int32.Parse(noteData[2]), 1, Int32.Parse(noteData[3]) & 11));
+                }
+            }
+            //timing value in file is milliseconds since map start
+            for (int i = notes.length; i > 0; i--)
+            {
+                //calculate time until next note
+                notes[i - 1].duration = notes[i].duration - notes[i - 1].duration;
+            }
+            //last note has no next note
+            notes[notes.length - 1].duration = 0;
+            return notes;
+        }
+
+        Note[] compress(Note[] notes) {
+            for (int i = notes.length; i > 0; i--;) {
+                //if 2 sequential notes are equal, group them together
+                if (notes[i] = notes[i - 1]) {
+                    notes[i - 1].quantity = notes[i].quantity++;
+                    notes.removeAt(i);
+                }
+            } 
+        }
+    }
+}
---
+++ osu-rhythmic-complexity/osu-rhythmic-complexity/Step.cs
@@ -0,0 +1,44 @@
+﻿namespace ppcalc
+{
+    enum NoteType 
+    {
+        circle = 1, slider = 2, spinner = 8
+    }
+
+    class Note
+    {
+        public double duration { get; set; }
+        public int quantity { get; set; }
+        public NoteType type { get; set; }
+
+        public Note(double _duration, int _quantity, NoteType _type) {
+            duration = _duration;
+            quantity = _quantity;
+            type = _type;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Note rhs = (Note)obj;
+            return this.duration == rhs.duration && this.type = rhs.type;
+        }
+
+        /*public static bool operator == (Note lhs, Note rhs)
+        {
+            if (object.ReferenceEquals(lhs, null))
+            {
+                return object.ReferenceEquals(rhs, null);
+            }
+            return lhs.Equals(rhs);
+        }
+
+        public static bool operator != (Note lhs, Note rhs)
+        {
+            if (object.ReferenceEquals(lhs, null))
+            {
+                return object.ReferenceEquals(rhs, null);
+            }
+            return !lhs.Equals(rhs);
+        }*/
+    }
+}
